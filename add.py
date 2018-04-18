afed92e0602e6c7df964fc85c0314c0b710740f3
user <user@fisat>
2018-04-18 04:56:19 -0400

    add four numbers

--- add.py
+++ add.py
@@ -4,8 +4,14 @@ def add2(n1, n2):
 
 print add2(3, 4)
 
-def sub(n1, n2):
-			s = n1 - n2
+def add3(n1, n2,n3):
+			s = n1 + n2+n3
 			return s
 
-print sub(5,4)
+print sub(5,4,1)
+ 
+def add4(n1, n2,n3,n4):
+			s = n1 + n2+n3+n4
+			return s
+
+print sub(5,4,8,6)
